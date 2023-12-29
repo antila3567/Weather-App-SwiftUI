4f27eb0b9ec6d2198fbf3264056b6406f2fe855e
IvanL <antila3567@gmail.com>
2023-12-29 16:49:19 +0200

    Initial Commit

---
+++ WeatherAppIl/WeatherAppIlApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppIlApp.swift
+//  WeatherAppIl
+//
+//  Created by Иван Легенький on 29.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppIlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
